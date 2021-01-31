2ccf051cd0ec77d4749334c3046851bb481dbc1a
MiaoXia0 <953003855@qq.com>
2021-01-31 23:21:44 +0800

    装备

--- Densetsunokuni.py
+++ Densetsunokuni.py
@@ -18,7 +18,7 @@ if not os.path.exists(user_dir):
     os.mkdir(user_dir)
 try:
     equipments = json.load(open(equip_dir + '/equipments.json', 'r'))
-except IOError:
+except FileNotFoundError:
     equipments = [
         {'id': 0, 'name': '无', 'atk': 0, 'def': 0, 'hp': 0, 'mp': 0, 'mov': 0},
                   {'id': 1, 'name': '普通的衣物', 'atk': 0, 'def': 5, 'hp': 10, 'mp': 0, 'mov': 0},
