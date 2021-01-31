af5abb7fc85247cd867d2f6485f23f7cf3a15ea1
MiaoXia0 <953003855@qq.com>
2021-01-31 18:39:34 +0800

    创建角色

--- cmds.py
+++ cmds.py
@@ -1,6 +1,23 @@
 from hoshino.typing import *
 from .DenSeTsuNoKuni import reg_cmd
 from .texts import *
+import os
+try:
+    import ujson as json
+except ImportError:
+    import json
+
+data_dir = os.path.dirname(__file__) + '/data'
+user_dir = data_dir + '/users'
+
+
+def isUserExist(QQ: str):
+    try:
+        json.load(open(user_dir + f'/{QQ}.json', 'r'))
+    except IOError:
+        return False
+    else:
+        return True
 
 
 @reg_cmd('测试输出')
@@ -17,3 +34,18 @@ async def cmd_desc(bot: HoshinoBot, ev: CQEvent, args):
 @reg_cmd(['菜单', 'menu'])
 async def cmd_menu(bot: HoshinoBot, ev: CQEvent, args):
     await bot.send(ev, menu)
+
+
+@reg_cmd('创建角色')
+async def cmd_create(bot: HoshinoBot, ev: CQEvent, args):
+    name = args[0]
+    user_id = ev['user_id']
+    if name == '':
+        await bot.send(ev, '请输入//创建角色 角色名')
+        return
+    if isUserExist(user_id):
+        await bot.send(ev, '您已创建角色！')
+        return
+    new_user = {'name': name, 'level': 0, 'exp': 0}
+    json.dump(open(user_dir + f'{user_id}.json', 'w'))
+    await bot.send(ev, f'角色{name}已创建，欢迎来到传说的国度！')
